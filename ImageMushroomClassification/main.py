7f154aefaff685e8e2f5980c40ed58015d2b3bbf
Eli Orians <eli.orians@gmail.com>
2024-10-24 17:34:13 -0400

    some experimenting

--- ImageMushroomClassification/main.py
+++ ImageMushroomClassification/main.py
@@ -1,19 +1,23 @@
 import os
-import PIL.Image
+import pickle
 import numpy as np
 import pandas as pd
 
-#machine learning imports
-from sklearn.calibration import LabelEncoder
+#encoding imports
+import PIL.Image
+from sklearn.preprocessing import LabelEncoder
+from ast import literal_eval
+#model training imports
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+#evaluation imports
 from sklearn.metrics import accuracy_score, confusion_matrix
-
 #plotting imports
 import seaborn as sns
 from matplotlib import pyplot as plt
 
 def load_images(folder):
+    print("loading images...")
     #initialize list of images and labels
     images = []
     labels = []
@@ -33,6 +37,7 @@ def load_images(folder):
     return pd.DataFrame({'image': images, 'label' : labels})
 
 def process_images(image_paths, target_size=(128, 128)):
+    print("processing images...")
     images_encoded = []
     #open each image
     for img_path in image_paths:
@@ -47,17 +52,54 @@ def process_images(image_paths, target_size=(128, 128)):
 
 def main(): 
 
-    # build the dataframe of images and labels
-    data = load_images('ImageMushroomClassification\data\data')
-
-    #encode images (turn image to array of pixels and flatten to one dimensional array)
-    data['images_encoded'] = process_images(data['image'])
-
-    #encode labels
+    #init encoder
     le = LabelEncoder()
-    data['label_encoded'] = le.fit_transform(data['label'])
 
+    if os.path.exists('ImageMushroomClassification/encoded_data.pkl'):
+        with open('ImageMushroomClassification/encoded_data.pkl', 'rb') as file:
+            data = pickle.load(file)
+
+        print('encoded data loaded.')
+
+    else:
+        # build the dataframe of images and labels
+        data = load_images('ImageMushroomClassification\data\data')
+
+        #encode images (turn image to array of pixels and flatten to one dimensional array)
+        data['images_encoded'] = process_images(data['image'])
+
+        #encode labels
+        data['label_encoded'] = le.fit_transform(data['label'])
+
+        #save dataframe to format suitable for encoded images
+        with open('ImageMushroomClassification/encoded_data.pkl', 'wb') as file:
+            pickle.dump(data, file)
+
+        print('encoded data saved.')
+
+    #reduce dataset for experimenting
+    data = data.sample(frac=0.005)
+
+    print("the processed data:")
     print(data)
+
+    #set up "x" and "y" columns
+    #convert encoded images to 2d numpy array for training
+    x = np.stack(data['images_encoded'].values)
+    y = data['label_encoded']
+
+    #TODO split data into training and testing
+
+    #TODO choose a model (start with logistic regression)
+
+    #TODO fit the model
+
+    #TODO generate predictions
+
+    #TODO evaluate predictions
+
+    #TODO plot confusion matrix
+
 
 #running main
 if __name__ == "__main__":
